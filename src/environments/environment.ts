e307833a8bbcc3615f161183a8515074f610b0fa
Yalcin Emilli <97788204+yalcinemilli@users.noreply.github.com>
2024-06-20 14:42:10 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,13 +6,13 @@
 // If left as is, it firbase and google map related functionality will not work on LIVE instance.
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: 'AIzaSyAvHtu5rrqKSZQg75R2MYqlNgGdMKy2YR4',
+  /*firebase: {
+    apiKey: '*********',
     projectId: 'modern-admin-8453e',
     authDomain: 'modern-admin-8453e.firebaseapp.com',
     appId: '1:300550201463:web:96a271cc309ac5aea86672'
-  },
-  googleApiKey: 'AIzaSyAIIYOxA7qeetFz6TuR1Qewc0Rrjhzx7ZU'
+  },*/
+  googleApiKey: '*********'
 };
 
 /*
